249d83c6945db2c36725de698746964abbabdd3f
deweesa <asadeweese@gmail.com>
2026-06-23 17:13:05 -0700

    Fresh start

--- src/app/components/About.js
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return(
-    <p id="about" className="mb-6">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus non enim praesent elementum facilisis leo. Mattis ullamcorper velit sed ullamcorper. Diam ut venenatis tellus in metus vulputate eu scelerisque felis. Enim nunc faucibus a pellentesque sit amet porttitor eget. Bibendum est ultricies integer quis auctor elit sed vulputate mi. Duis at tellus at urna condimentum mattis pellentesque. Pharetra massa massa ultricies mi quis hendrerit. Risus pretium quam vulputate dignissim suspendisse in. Turpis in eu mi bibendum neque egestas congue. Ac tortor dignissim convallis aenean et tortor at risus viverra. Vel orci porta non pulvinar. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc. Nam at lectus urna duis convallis convallis. Non quam lacus suspendisse faucibus.
-    </p>
-  )
-}
--- src/app/components/Spotlight.js
+++
@@ -1,9 +0,0 @@
-export default function Spotlight() {
-  return (
-    <div className="mx-auto">
-      <h2 className="text-4xl font-bold tracking-tight">Asa DeWeese</h2>
-      <h3 className="text-lg">Backend Engineer</h3>
-      Hi, I'm Asa and nvim is giving me some swagger
-    </div>
-  )
-}
